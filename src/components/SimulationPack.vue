65d8ef83bc527458773a9e7ddfc304bff8691185
Remake1 <68705597+Remake1@users.noreply.github.com>
2023-03-02 11:04:04 +0200

    Changed document.onclick to vue @click

--- src/components/SimulationPack.vue
+++ src/components/SimulationPack.vue
@@ -1,20 +1,18 @@
 <script setup>
-    import { ref, onMounted } from 'vue'
-    import * as graph from "@/lib/graph";
-    onMounted(() => {
-        document.getElementById("add_particles_small").onclick = _ => {
-            graph.addMoleculeType(100,4,5,[125,80,80]);
-        };
-        document.getElementById("add_particles_big").onclick = _ => {
-            graph.addMoleculeType(100,4,12,[80,80,125]);
-        };
-    });
+import * as graph from "@/lib/graph";
+
+function add_particles_small(){
+  graph.addMoleculeType(100,4,5,[125,80,80]);
+}
+function add_particles_big(){
+  graph.addMoleculeType(100,4,12,[80,80,125]);
+}
 </script>
 
 <template>
-    <div class="simulation">
-        <canvas id="canvas_mkt_basic" width="800" height="600"></canvas>
-    </div>
-    <button id="add_particles_small">Додати маленьких частинок</button>
-    <button id="add_particles_big">Додати великих частинок</button>
+  <div class="simulation">
+    <canvas id="canvas_mkt_basic" width="800" height="600"></canvas>
+  </div>
+  <button @click="add_particles_small" id="add_particles_small">Додати маленьких частинок</button>
+  <button @click="add_particles_big" id="add_particles_big">Додати великих частинок</button>
 </template>
